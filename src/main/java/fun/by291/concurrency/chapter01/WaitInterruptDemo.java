078e58dd79cfe6662e12b5f0d8d4ff3b74502f92
Bystander <2823197499@qq.com>
2022-01-06 15:33:56 +0800

    Update 更新 WaitInterruptDemo 添加了一些注释

--- src/main/java/fun/by291/concurrency/chapter01/WaitInterruptDemo.java
+++ src/main/java/fun/by291/concurrency/chapter01/WaitInterruptDemo.java
@@ -2,6 +2,12 @@ package fun.by291.concurrency.chapter01;
 
 /**
  * wait() 和 interrupt() 方法示例
+ * <br>
+ * <br>
+ * <b>线程 A 调用了 wait() 进入了等待状态，可以用 interrupt() 取消。不
+ * 过这时候要注意锁定的问题。线程在进入等待区,会把锁定解除,当对等待中的
+ * 线程调用 interrupt() 时，会先重新获取锁定，再抛出异常。在获取锁定之前，
+ * 是无法抛出异常的。</b>
  *
  * @author Bystander
  */
@@ -21,6 +27,7 @@ public class WaitInterruptDemo {
                     // 由于 thread 一直阻塞在 synchronized 块中，而后 thread 又被
                     // 中断，所以 thread 会在调用 wait() 方法的地方抛出 InterruptedException，
                     // 然后进入 catch 块中，wait() 以下的代码都不会到达
+                    // 抛出 InterruptedException 的同时会清除中断状态
                 }
                 System.out.println("退出 synchronized 块");
             } catch (InterruptedException e) {
@@ -33,7 +40,7 @@ public class WaitInterruptDemo {
         Thread.sleep(1000);
 
         System.out.println("调用 thread 的 interrupt() 方法");
-        // interrupt() 方法只是设置了中断状态
+        // interrupt() 方法只是设置了中断状态，并不会中断一个正在正常运行的线程
         // 当线程由于调用了 wait(), join() 和 sleep() 方法及其重载方法中的其中一个
         // 而阻塞时，线程会立即抛出一个 InterruptedException，进而退出阻塞状态
         thread.interrupt();
