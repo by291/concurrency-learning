e05c1a43b927505b4b1b1903d19d412ffb137650
Bystander <2823197499@qq.com>
2022-01-07 22:09:25 +0800

    Add 添加 ThreadUnsafeIntegerDemo

---
+++ src/main/java/fun/by291/concurrency/chapter02/ThreadUnsafeIntegerDemo.java
@@ -0,0 +1,21 @@
+package fun.by291.concurrency.chapter02;
+
+/**
+ * <p>线程不安全的类举例</p>
+ * <p>当有多个线程同时操作时，就有可能读不到最新值</p>
+ *
+ * @author Bystander
+ *
+ * @see package-info.java
+ */
+public class ThreadUnsafeIntegerDemo {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
