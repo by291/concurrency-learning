42d1c2e94ff867f67d002d794591fe6d0289e67f
Bystander <2823197499@qq.com>
2022-01-10 23:06:28 +0800

    Update 修改 注释内容

--- src/main/java/fun/by291/concurrency/chapter02/TraditionalRandomDemo.java
+++ src/main/java/fun/by291/concurrency/chapter02/TraditionalRandomDemo.java
@@ -3,11 +3,11 @@ package fun.by291.concurrency.chapter02;
 import java.util.Random;
 
 /**
- * 线程不安全的 Random 类使用示例
+ * 传统的 Random 类使用示例
  *
  * @author Bystander
  */
-public class ThreadUnsafeRandomDemo {
+public class TraditionalRandomDemo {
 
     public static void main(String[] args) {
         // 创建一个包含默认种子的随机数生成器
@@ -18,8 +18,11 @@ public class ThreadUnsafeRandomDemo {
             // 1.首先根据老的种子生成新的种子
             // 2.根据新的种子来生成新的随机数
             //
-            // 这不是一个具有原子性的过程
-            // 根据这个原理，Random 类在多线程环境下就有可能多个线程生成同一个值
+            // 这不是一个原子性的过程，但是 JDK 也考虑到了这点，通过 CAS 和一个
+            // 原子变量防止多线程环境下多个线程拿到同一个老种子，生成相同的随机数
+            //
+            // 但是这个是有局限性的，详解请看我的另一个项目: jdk-source
+            // 详细注释都在源码里
             System.out.println(random.nextInt(5));
         }
     }
